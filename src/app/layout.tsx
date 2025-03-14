ce5af50882c402f56a98d31a58d37b5a3d77d754
Swayam <swayambansal@outlook.com>
2025-03-14 19:40:03 +0530

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   subsets: ["latin"],
